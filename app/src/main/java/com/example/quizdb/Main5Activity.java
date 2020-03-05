fdff5dc87b8ebd0beb357d77b34e1dfd9089a3f2
Nithin4536 <48560733+Nithin4536@users.noreply.github.com>
2020-03-05 10:43:20 -0500

    Retreving data from Firebase .

--- app/src/main/java/com/example/quizdb/Main5Activity.java
+++ app/src/main/java/com/example/quizdb/Main5Activity.java
@@ -1,14 +1,56 @@
-package com.example.quizdb;
+package com.example.quizproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Main5Activity extends AppCompatActivity {
-
+DatabaseReference databaseReference;
+Button b1;
+TextView t1,t2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        b1=findViewById(R.id.button4);
+        t1=findViewById(R.id.textView2);
+        t2=findViewById(R.id.textView3);
+
+
+        databaseReference=FirebaseDatabase.getInstance().getReference().child("Players");
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference=FirebaseDatabase.getInstance().getReference().child("Players");
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String fname=dataSnapshot.child("fname").getValue().toString();
+                        String fscore=dataSnapshot.child("fscore").getValue().toString();
+                        t1.setText(fname);
+                        t2.setText(fscore);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
     }
-}
+    }
