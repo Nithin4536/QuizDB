cae292e9baf5ad59654ab1a72f11edcdd8898f8a
Nithin4536 <48560733+Nithin4536@users.noreply.github.com>
2020-03-05 10:45:27 -0500

    Retreving data from Firebase using list and Arrayadapter.

--- app/src/main/java/com/example/quizdb/Main6Activity.java
+++ app/src/main/java/com/example/quizdb/Main6Activity.java
@@ -1,14 +1,66 @@
-package com.example.quizdb;
+package com.example.quizproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class Main6Activity extends AppCompatActivity {
+    DatabaseReference databaseReference;
+    ListView mlistview;
+    RelativeLayout rl;
+    ArrayList<String> musername=new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+        databaseReference= FirebaseDatabase.getInstance().getReference().child("Players");
+        mlistview=(ListView) findViewById(R.id.rd);
+        rl=findViewById(R.id.rl);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item,musername);
+        mlistview.setAdapter(arrayAdapter);
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value=dataSnapshot.getValue(String.class);
+                musername.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
